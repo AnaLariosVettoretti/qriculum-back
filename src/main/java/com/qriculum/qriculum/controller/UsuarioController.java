246a17b4d61b10cbba6b17382c6dde23e3f40d93
AnaLariosVettoretti <anavettoretti98@gmail.com>
2022-06-03 14:40:26 +0200

    Cambio en servicio validate

--- src/main/java/com/qriculum/qriculum/controller/UsuarioController.java
+++ src/main/java/com/qriculum/qriculum/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.qriculum.qriculum.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,46 +27,50 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@GetMapping("")
 	List<Usuario> index() {
 		return usuarioRepository.findAll();
 	}
-	
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("")
 	Usuario create(@RequestBody Usuario usuario) {
-		
+
 		usuario.setContrasenia(Cifrado.getMD5(usuario.getContrasenia()));
 		return usuarioRepository.save(usuario);
 	}
 
-	@GetMapping( path = "/{id}")
-    Usuario getOne(@PathVariable("id") String username) {
-        return this.usuarioRepository.findById(username).orElseThrow(RuntimeException::new);
-    }
-	
+	@GetMapping(path = "/{id}")
+	Usuario getOne(@PathVariable("id") String username) {
+		return this.usuarioRepository.findById(username).orElseThrow(RuntimeException::new);
+	}
 
-	@GetMapping( path = "/{id}{contrasenia}")
-    Usuario validateUser(@PathVariable("id") String username, String password) {
+	@GetMapping(value = "/user")
+	public Usuario validateUser(
+
+	    @RequestParam(value = "id", required = true) String username,
+		@RequestParam(value = "hash", required = true) String password
+	    ) {
 		
-        Usuario usuarioBBDD = this.usuarioRepository.findById(username).orElseThrow(RuntimeException::new);
-        
-        String inputPass = Cifrado.getMD5(password);
-        
-        if (usuarioBBDD.getContrasenia() == inputPass) {
-			return usuarioBBDD;
-		}else {
-			Usuario userVacio = new Usuario();
-			return userVacio;
-		}
-		
+	    Usuario usuarioBBDD = this.usuarioRepository.findById(username).orElseThrow(RuntimeException::new);
+	        
+	    return usuarioBBDD;
+	    /*    
+	    String inputPass = Cifrado.getMD5(password);
+	        
+	        if (usuarioBBDD.getContrasenia() == inputPass) {
+				return usuarioBBDD;
+			}else {
+				Usuario userVacio = new Usuario();
+				return userVacio;
+			}
 			
-    }
-    
-	
+		*/	
+	}
+
 	@PutMapping("{id}")
-	Usuario update(@PathVariable String username, @RequestBody Usuario usuario) {		
+	Usuario update(@PathVariable String username, @RequestBody Usuario usuario) {
 		Usuario usuarioBBDD = usuarioRepository.findById(username).orElseThrow(RuntimeException::new);
 		usuarioBBDD.setUsuario(usuario.getUsuario());
 		usuarioBBDD.setNombre(usuario.getNombre());
@@ -76,16 +80,16 @@ public class UsuarioController {
 		usuarioBBDD.setContrasenia(usuario.getContrasenia());
 		usuarioBBDD.setTelefono1(usuario.getTelefono1());
 		usuarioBBDD.setTelefono2(usuario.getTelefono2());
-		usuarioBBDD.setCiudad(usuario.getCiudad());		
+		usuarioBBDD.setCiudad(usuario.getCiudad());
 
 		return usuarioRepository.save(usuarioBBDD);
 	}
-	
+
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("{id}")
 	void delete(String username) {
 		Usuario usuarioBBDD = usuarioRepository.findById(username).orElseThrow(RuntimeException::new);
-		
+
 		usuarioRepository.delete(usuarioBBDD);
 	}
 }
--- src/main/java/com/qriculum/qriculum/repository/UsuarioRepository.java
+++ src/main/java/com/qriculum/qriculum/repository/UsuarioRepository.java
@@ -1,10 +1,16 @@
 package com.qriculum.qriculum.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import com.qriculum.qriculum.beans.Usuario;
 
 
 
 public interface UsuarioRepository extends MongoRepository<Usuario, String>{
 
+	@Query("{usuario: ?0, contrasenia: ?1}")
+    //@Query("{$and :[{author: ?0},{cost: ?1}] }")
+    Usuario getUsuarioByUsuarioAndContrasenia(String usuario, String contrasenia);
+	
 }
